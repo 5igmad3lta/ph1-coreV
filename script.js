9d502af9d990648f76d84efa4adc0b185f2b73f0
Stavros Droulias <stavros.d27@gmail.com>
2025-06-01 16:46:55 +1000

    Add renderDefault() function to test retrieval and render of attribute to DOM

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
     fetch("http://localhost:3000/exercises")
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => renderDefault(data))
 });
+
+function renderDefault(data) {
+  const list = document.getElementById('exercise-list');
+  list.innerHTML = "";
+  data.forEach((exercise) => {
+    let item = document.createElement('li');
+    item.textContent = exercise.name.toUpperCase();
+    item.classList = "item";
+    item.id = exercise.id;
+    list.appendChild(item);
+  })
+}
